9dfd64f060200ba7d1e5b5aafacf7b07e9920791
Stk <shuheitakahashi0822@gmail.com>
2024-09-13 12:03:54 +0900

    💩 ナビゲーションにプロフィールリンクを強制的に出す。

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -44,6 +44,7 @@ const Navigation = ({
               </Link>
             </div>
           ) : (
+            // ここで無理やりプロフィールへのリンク設定・sessionはnullだが...
             <div className="flex items-center space-x-5">
               <div className="flex items-center space-x-5">
                 <Link href="/setting/profile">
